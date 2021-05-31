ec4cec3fef1f276d261f8c3f077e4b39fb87a6d6
jcalzateb <54165082+jcalzateb@users.noreply.github.com>
2021-05-31 04:47:57 -0500

    The reactive form was implemented

--- src/app/pages/list-institutions/list-institutions.component.ts
+++ src/app/pages/list-institutions/list-institutions.component.ts
@@ -16,8 +16,8 @@ export class ListInstitutionsComponent implements OnInit {
 
   ngOnInit(): void {
     this.institutionL=this.storageS.getList();
-    
   }
+  
   deleteInstitution(listI:Partner){
     if(confirm('Are you sure you want to delete it?')){
       this.storageS.deleteInstitution(listI);
--- src/app/pages/register-partner/register-partner.component.ts
+++ src/app/pages/register-partner/register-partner.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormControl, FormGroup, Validators, FormBuilder } from '@angular/forms';
 import { Router } from '@angular/router';
 import { LocalStorageService } from '../../services/local-storage.service';
 import { Partner, Country } from '../../models/partner.interface';
@@ -15,7 +15,11 @@ export class RegisterPartnerComponent implements OnInit {
   public countryI: any[2740];
   institutionF: Partner[];
 
-  constructor(private router: Router, private storageS:LocalStorageService) { }
+  constructor(private router: Router, private storageS:LocalStorageService) {
+    this.rForm.valueChanges.subscribe(value=>{
+      console.log(value);
+    });
+   }
 
   ngOnInit(): void {
     this.rForm.get("institution").setValue('YES');
@@ -29,15 +33,21 @@ export class RegisterPartnerComponent implements OnInit {
   rForm = new FormGroup({
     institution: new FormControl('', Validators.required),
     acronym: new FormControl('', [Validators.required,Validators.maxLength(10)]),
-    name: new FormControl('', [Validators.required, Validators.minLength(10)]),
+    name: new FormControl('', [Validators.required, Validators.maxLength(60)]),
     type: new FormControl('', Validators.required),
     country: new FormControl('', Validators.required),
     city: new FormControl('',Validators.required),
     website: new FormControl('http://')
   });
 
-  send(rForm:Partner){
-    this.storageS.addIstitution(rForm);
+
+
+  saveF(form:Partner){
+    if (this.rForm.valid) {
+      this.storageS.addIstitution(form);
+    } else {
+      this.rForm.markAllAsTouched();
+    }
     //console.log(this.storageS.getList);
   }
 
